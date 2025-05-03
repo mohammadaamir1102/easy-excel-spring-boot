3840a1aa3b513d7161350820bf0d47bceb8f7ef2
mohammadaamir1102 <er.aamir1102@gmail.com>
2025-05-03 18:29:30 +0300

    initial commit

--- src/main/java/com/aamir/repo/StudentRepo.java
+++ src/main/java/com/aamir/repo/StudentRepo.java
@@ -2,8 +2,6 @@ package com.aamir.repo;
 
 import com.aamir.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-//@Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
 }
